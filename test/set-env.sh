5a9f8bd4a207be5adb8bf9870a8b241b862e4583
Siddharth Yadav <siddharth16csu361@ncuindia.edu>
2020-07-12 21:21:50 -0700

    LR 1 test 3

--- test/set-env.sh
+++ test/set-env.sh
@@ -1,6 +1,8 @@
  
 docker run -p 8888:8888 -d --name jupyter jupyter/scipy-notebook:83ed2c63671f
 
-#docker restart jupyter
+docker restart jupyter
 
-#docker logs jupyter
+docker cp ./ jupyter:/home/jovyan/work
+
+docker exec -it jupyter bash -c 'jupyter notebook list' | cut -d'=' -f 2 | cut -d' ' -f 1
