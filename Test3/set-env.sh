8e5e75cc8ef024dad79ee7c73c0d4130c1ab3f06
Siddharth Yadav <sidyadav2009@gmail.com>
2020-11-03 01:07:47 +0530

    D

--- Test3/set-env.sh
+++ Test3/set-env.sh
@@ -1,4 +1,4 @@
-apt install bsdtar -y
-curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/python/2020.10.332292344/vspackage | bsdtar -xvf - ms-python
-mv extension /opt/.katacodacode/extensions/mspython
+#apt update && apt install bsdtar -y
+#curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/python/2020.10.332292344/vspackage | bsdtar -xvf - ms-python
+#mv extension /opt/.katacodacode/extensions/mspython
 pip3 install jupyter notebook
